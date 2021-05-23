1daf157e2fb98fa5c5682bfa016dcd8d803c47df
M Vendors <m-vendors@protonmail.com>
2021-05-23 20:48:54 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'codeway_DummyPkg',
+    __DIR__
+);
